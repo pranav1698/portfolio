69df151860b8a6387b169c6dea7516e925ab46af
Pranav Kulshrestha <hulkcodexl16@gmail.com>
2018-12-20 21:41:07 +0530

    Tested php

---
+++ header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="My Portfolio">
+    <meta name="author" content="Pranav">
+    <title>My Portfolio</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Add custom CSS here -->
+	
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/ekko-lightbox.css" rel="stylesheet">
+    <link href="css/flexslider.css" rel="stylesheet">
+    <link href="css/animate.min.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+		<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+  </head>
+
+  <body>
+  <div id="boxWrapp">
+    <div class="main-nav clearfix">
+	  <!-- navbar -->
+	<nav class="navbar navbar-inverse" role="navigation">
+        <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#NavCol">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand bg-primary" href="#about">Pranav</a>
+        </div>
+        
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="NavCol">
+         <ul class="nav navbar-nav navbar-right">
+           
+            <li class="current"><a href="#about" class="linear">About Me</a></li>
+            <li><a href="#" class="linear">Biography</a></li>
+            <li><a href="#work" class="linear">Resume</a></li>
+            <li><a href="#contact" class="linear">Contact</a></li>
+            <li><a href="#contact" class="linear">Portfolio</a></li>
+            <li><a href="#contact" class="linear">Hire Me</a></li> 
+          </ul>
+        
+         
+        </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+	  
+	</div>
